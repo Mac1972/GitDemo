68c24a2621166d9e2640047c2fc0583879ab8ab9
Mac <honeyhome1972@yahoo.co.jp>
2019-05-09 21:03:04 +0800

    new5

--- TestSelenium.py
+++ TestSelenium.py
@@ -6,17 +6,19 @@ import time
 driver = webdriver.Chrome() # 趨動放進這個資料夾 C:\Users\Mac\AppData\Local\Programs\Python\Python37-32\Scripts
 #driver = webdriver.Firefox() # 趨動放進這個資料夾 C:\Users\Mac\AppData\Local\Programs\Python\Python37-32\Scripts
 
+driver.maximize_window()
+
 #driver.fullscreen_window()
 
 #driver.get('http://www.baidu.com')
 
 #driver.get('http://www.douban.com')
 
-driver.get('http://www.google.com')
+#driver.get('http://www.google.com')
 
 #driver.get('http://www.amazon.com')
 
-#driver.get('http://www.timeanddate.com')
+driver.get('http://www.timeanddate.com')
 
 #driver.find_element_by_id('nav-your-amazon').click()
 
@@ -24,7 +26,7 @@ driver.get('http://www.google.com')
 
 #driver.find_element_by_name('q').send_keys('Mac\n') #  下一行 \n 代表將字輸入搜尋
 
-driver.find_element_by_link_text('Gmail').click()
+#driver.find_element_by_link_text('Gmail').click()
 
 #driver.find_element_by_class_name('lnk-movie').click()
 
@@ -52,21 +54,25 @@ driver.find_element_by_link_text('Gmail').click()
 
 
 
-'''selectElements = driver.find_element_by_id('month')
+
+selectElements = driver.find_element_by_id('month')
 months = Select(selectElements)
 months.select_by_visible_text('January')
 countriesElements = driver.find_element_by_id('country')
 counteries = Select(countriesElements)
 counteries.select_by_visible_text('Taiwan')
-driver.find_element_by_xpath("//div[4]//input[1]").click()'''
+driver.find_element_by_xpath("//div[4]//input[1]").click()
 
-'''driver.find_element_by_name('q').send_keys('Mac\n')
+
+'''
+driver.find_element_by_name('q').send_keys('Mac\n')
 
 driver.find_element_by_class_name('LC20lb').click()
 
 driver.find_element_by_class_name('first-name').click()
 
-driver.find_element_by_id("form--signin--field--EMAIL_ADDRESS").send_keys("Mac")'''
+driver.find_element_by_id("form--signin--field--EMAIL_ADDRESS").send_keys("Mac")
+'''
 
 
 
